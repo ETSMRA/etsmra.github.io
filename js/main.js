402802a161de3a78bd3202ce24ea75f913dbb070
ETSMRA <escuelataller2026@gmail.com>
2026-05-27 12:51:35 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -50,3 +50,15 @@ const menu = document.getElementById('menu');
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+
+// Incorporado 27/05. Eventos para Dudas
+
+document.getElementById('icons1').addEventListener('click',function () { this.innerHTML='Programas de empleo y formación de carácter temporal, para mejorar la empleabilidad de jovenes desempleados/as mediante la cualificación en certificados de profesionalidad y la ejecución de obras de utilidad pública'; this.style.fontSize='small'});
+/*
+document.getElementById('icons1').addEventListener('mouseout',function () { this.innerHTML='¿Qué es una Escuela Taller?'; this.style.fontSize='large'});
+*/
+
+document.getElementById('icons2').addEventListener('click',function () { this.innerHTML='Centro de Educación de Adultos del Nalón <br>Palacio Valdés, s/n <br> Sotrondio 33950'; this.style.fontSize='small'});
+
+document.getElementById('icons3').addEventListener('click',function () { this.innerHTML='Comenzamos el 1 de abril de 2026 con la fase de formaci&oacute;n y posteriormente el 01 de julio de 2026 se comienza la fase de obra hasta el 31 de marzo de 2027'; this.style.fontSize='small'});
