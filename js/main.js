4af05294a7811ffaccf45f658f7b705eccf36ae6
ETSMRA <escuelataller2026@gmail.com>
2026-06-09 08:06:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,9 +45,7 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-          
-        }
+        } 
     });
 });
 
